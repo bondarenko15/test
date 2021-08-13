312d3213ca33f43d10b9679a046b35b63d0399b8
Anton <tonissstar058@gmail.com>
2021-08-13 16:54:27 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,40 @@
+/* "use strict";
+
+const str = "test";
+const arr = [10,20,30];
+
+console.log(str.length);
+console.log(arr.length);
+
+
+/* строчные методы */
+
+/* const logg = " Hello world";
+
+console.log(logg.slice(6, 8));
+
+console.log(logg.substring(6, 10));
+
+console.log(logg.substr(5, 10));
+ */
+/* 
+числовые методы  */
+
+/* const num = 15.5;
+
+console.log(Math.round(num));
+
+
+const test = "12.3px";
+ console.log(parseInt(test));
+
+
+
+
+ */
+
+
+
+
+
+
